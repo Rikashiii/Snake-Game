7f058fdb3c83aa913d5733294d1f158cc38afcd8
Harsh Chandrakar <harshtg9999@gmail.com>
2024-08-05 23:40:15 +0530

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,4 +1,26 @@
 from turtle import Turtle
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 def up(self):
+    if self.head.heading() != DOWN:
+        self.head.setheading(UP)
+
+
+def down(self):
+    if self.head.heading() != UP:
+        self.head.setheading(DOWN)
+
+
+def left(self):
+    if self.head.heading() != RIGHT:
+        self.head.setheading(LEFT)
+
+
+def right(self):
+    if self.head.heading() != LEFT:
+        self.head.setheading(RIGHT)
 
